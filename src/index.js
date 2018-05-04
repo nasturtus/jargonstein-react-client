1f300f8c5d0da664ade04a1226f7b41cbb04bcb8
nasturtus <nasturtus@gmail.com>
2018-05-04 17:21:13 +0800

    Remove reference to dotenv

--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from "react-dom";
 import AppRouter from "../src/Components/AppRouter";
 import { BrowserRouter } from "react-router-dom";
 
-// require("dotenv").config();
-// const dotenv = require('dotenv')
-// const result = dotenv.config()
- 
-// if (result.error) {
-//   throw result.error
-// }
- 
-// console.log(result.parsed)
-
 ReactDOM.render(
   <BrowserRouter>
     <AppRouter />
